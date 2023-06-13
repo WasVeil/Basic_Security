77adf990f72cee20da197c14e143997e1e61e533
Aviva <123884383+zAviva@users.noreply.github.com>
2023-06-12 22:45:39 -0500

    Create SecureTable.lua

---
+++ SecureTable.lua
@@ -0,0 +1,46 @@
+-- Made by @Aviva
+
+--[[
+	Pros:
+	- prevents the use of pairs/ipairs/next/foreach
+	- using newcclosure makes impossible to get constants/upvalues
+	- using numbers/letters/patterns prevents having an idea of what the function does. (ex: if the function is originally called "getplayers", you will have an idea of what it does, you can just randomize the index key.)
+
+	Cons:
+	- Doesn't detect tampers or hooks. Figure out yourself.
+	- Doesn't ensure uncrackeable code or 100% protected table.
+	- Probably getgc bypasses it, idk I didn't tested.
+]]--
+
+local protable = newproxy(true);
+
+local meta = getmetatable(protable)
+meta.__index = function(_, b)
+	local pt1 = newproxy(true);
+	local mt1 = getmetatable(pt1)
+    if b == "1" then -- imagine this function gets the players ok?
+        mt1.__call = function(_,b,c) -- use newcclosure instead.
+            print("FUNCION 1")
+			return c*b;
+        end;
+    end;
+	if b == "2" then
+        mt1.__call = function(_,b,c) -- use newcclosure instead.
+            print("FUNCION 2");
+			return b/c;
+        end;
+    end;
+	return pt1;
+end;
+
+-- basic test below.   (LUAU DEMO: https://luau-lang.org/demo)
+
+local o1,o2 = 2,5;
+
+local n1 = protable["1"](o1,o2); -- 2*5
+local n3 = protable["2"](n1,o2);  -- 2*5/5
+
+print(o1,o2); -- output: 2,5
+print("----------");
+print(n1);  -- output: 10
+print(n3);  -- output: 2
